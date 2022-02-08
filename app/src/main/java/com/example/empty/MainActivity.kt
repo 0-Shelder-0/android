156639ef646d7b4bbe1fd180f3e28bd72bf2d232
0-Shelder-0 <vladpiligrimov@gmail.com>
2022-02-08 20:00:43 +0500

    all done

--- app/src/main/java/com/example/empty/MainActivity.kt
+++ app/src/main/java/com/example/empty/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.empty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = findViewById<TextView>(R.id.textView)
+        val student = Student("Vasya", 2)
+        view.text = student.toString()
+    }
+}
+
+data class Student(
+    val name: String,
+    val course: Int) {
+    override fun toString(): String {
+        return "Student(name='$name', course=$course)"
     }
 }
